4fa3427dc646281b6d2d19b817dc06b11b02093e
AnasElsheik <121642047+AnasElsheik@users.noreply.github.com>
2022-12-30 00:04:24 +0200

    Add files via upload

---
+++ perfect numbers.cs/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+            if (n1 == 0)
+                n1++;
+            int n, i, sum;
+            for(n =n1; n <= n2; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.Write("{0} ", n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
